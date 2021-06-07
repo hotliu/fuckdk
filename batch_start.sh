a312beaeff4630cd4caa39f6e5ff2551d44fe1df
ZYMa <317958662@qq.com>
2021-06-08 00:06:46 +0800

    批量观看的shell

--- batch_start.sh
+++ batch_start.sh
@@ -2,5 +2,5 @@ idxstart=1486
 idxend=1509
 for((idx=$idxstart;idx<=$idxend;idx++));  
 do   
-python post_apply.py -url http://dxonline.ruc.edu.cn/index.php\?s\=/Index/vedio_cont/id/$idx.html
+python start.py -url http://dxonline.ruc.edu.cn/index.php\?s\=/Index/vedio_cont/id/$idx.html
 done
