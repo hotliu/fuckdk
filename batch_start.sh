e717ba45185da8fe4f107cd713b233442516776a
ZYMa <317958662@qq.com>
2021-06-10 18:24:04 +0800

    Update batch_start.sh

--- batch_start.sh
+++ batch_start.sh
@@ -1,5 +1,5 @@
 idxstart=1486
-idxend=1509
+idxend=1540
 for((idx=$idxstart;idx<=$idxend;idx++));  
 do   
 python start.py -url http://dxonline.ruc.edu.cn/index.php\?s\=/Index/vedio_cont/id/$idx.html
